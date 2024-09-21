524065927cf6d717caae70fc3a48d038f5ae34e1
ash02git <ashwinabraham2001@gmail.com>
2024-09-22 00:30:39 +0530

    Fall DEalth

---
+++ Assets/Scripts/FallDeathController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FallDeathController : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerController>() != null)
+        {
+            Debug.Log("Player Fell to Death");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
