1c709478ed7ed26c1f01440df0967d6d27ae4c8d
ash02git <ashwinabraham2001@gmail.com>
2024-09-22 00:14:39 +0530

    Added tilemaps

---
+++ Assets/Scripts/LevelEndController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelEndController : MonoBehaviour
+{
+    public string nextScene;
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.GetComponent<PlayerController>() != null)
+        {
+            Debug.Log("LEvelcompleted");
+            SceneManager.LoadScene(nextScene);
+        }
+    }
+}
